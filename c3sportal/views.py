e816d9cb701cd4de35906b99f394f3cc028b75cc
Christoph Scheid <c@shri.de>
2012-01-10 14:22:35 +0100

    basic views

--- c3sportal/views.py
+++ c3sportal/views.py
@@ -10,3 +10,30 @@ from .models import (
 def my_view(request):
     one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
     return {'one': one, 'project': 'C3S'}
+
+
+@view_config(route_name='definition', renderer='templates/definition.pt')
+def about_view(request):
+    return {'project': 'C3S'}
+
+
+@view_config(route_name='background', renderer='templates/background.pt')
+def background_view(request):
+    return {'project': 'C3S'}
+
+
+@view_config(route_name='motivation', renderer='templates/motivation.pt')
+def motivation_view(request):
+    return {'project': 'C3S'}
+
+
+@view_config(route_name='goals', renderer='templates/goals.pt')
+def goals_view(request):
+    one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
+    return {'one': one, 'project': 'C3S'}
+
+
+@view_config(route_name='roadmap', renderer='templates/roadmap.pt')
+def roadmap_view(request):
+    one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
+    return {'one': one, 'project': 'C3S'}
