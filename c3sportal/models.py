95757b53d6c5bccc869a09514e80c12b4b313298
Christoph Scheid <c@shri.de>
2012-01-21 05:01:10 +0100

    not using sqla atm

--- c3sportal/models.py
+++ c3sportal/models.py
@@ -16,13 +16,13 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
+
 class MyModel(Base):
     __tablename__ = 'models'
     id = Column(Integer, primary_key=True)
     name = Column(Text, unique=True)
     value = Column(Integer)
 
-    def __init__(self, name, value):
+    def __init__(self, name, value):  # pragma: no cover
         self.name = name
         self.value = value
-
