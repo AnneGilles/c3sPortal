3f204fbe5be2716f2d9ab5bbd85b2597d5bef7bc
Christoph Scheid <c@shri.de>
2012-01-21 05:00:33 +0100

    not using sqla atm

--- c3sportal/__init__.py
+++ c3sportal/__init__.py
@@ -7,8 +7,8 @@ from .models import DBSession
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
+    #engine = engine_from_config(settings, 'sqlalchemy.')
+    #DBSession.configure(bind=engine)
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
 
